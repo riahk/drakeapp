33b71cc0e1da70e0161c7950953a75353a6cc23d
riahk <moar.riah@gmail.com>
2015-05-22 17:01:47 -0700

    add s3 sdk

--- drakeapp/www/js/requestFactory.js
+++ drakeapp/www/js/requestFactory.js
@@ -2,10 +2,10 @@ angular.module('drakeApp', [])
 .factory('Favors', function ($http, $location){
   return {
     //return all the favors within range of a location (sent as an obj)
-    getFavors: function(loc) { 
+    getFavors: function(loc, callback) { 
       $http.post('/api/requests', loc)
         .success(function(data, status, headers, config) {
-          //return the requests
+          callback(data);
         })
         .error(function(data, status, headers, config) {
           //handle error
