1bda422f3c78a319a165ccbd6ce3c2491ff86069
riahk <moar.riah@gmail.com>
2015-05-23 09:29:02 -0700

    fix typo

--- server/photos/photoController.js
+++ server/photos/photoController.js
@@ -1,7 +1,7 @@
 // var Photo = require('./photoModel.js');
 // var Q = require('q');
-var aws = require('aws-sdk');
-var s3 = new aws.s3();
+//var aws = require('aws-sdk');
+//var s3 = new aws.s3();
 
 module.exports = {
   createPhoto: function(req, res, next) {
